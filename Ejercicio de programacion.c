f38cd38fc6b609b739c1ce3dc395d704e5b0bb8e
JahirManzaba <jahir.manzaba@udla.edu.ec>
2026-03-29 15:52:38 -0500

    Ejercicio de programación semana 1

---
+++ Ejercicio de programacion.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int inicio, fin, incremento;
+
+    printf("Ingrese el numero inicial: ");
+    scanf("%d", &inicio);
+
+    printf("Ingrese el numero final: ");
+    scanf("%d", &fin);
+
+    printf("Ingrese el valor de incremento: ");
+    scanf("%d", &incremento);
+
+    if (incremento == 0) {
+        printf("El incremento no puede ser 0.\n");
+        return 1;
+    }
+
+    if (inicio <= fin) {
+        for (int i = inicio; i <= fin; i += incremento) {
+            printf("%d\n", i);
+        }
+    } else {
+        for (int i = inicio; i >= fin; i += incremento) {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
